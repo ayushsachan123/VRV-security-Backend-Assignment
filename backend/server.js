1ff5b9199e646b354d0056df30748a2e8761fcd3
ayushsachan123 <ayushsachan.in@gmail.com>
2024-11-27 11:39:54 +0530

    add server.js

---
+++ backend/server.js
@@ -0,0 +1,62 @@
+require('dotenv').config()
+require('express-async-errors')
+const express = require('express')
+const app = express()
+const path = require('path')
+const errorHandler = require('./middleware/errorHandler')
+const cookieParser = require('cookie-parser')
+const cors = require('cors')
+const connectDB = require('./config/dbConn')
+const mongoose = require('mongoose')
+const PORT = process.env.PORT || 3500
+
+connectDB()
+
+
+// app.use(cors(corsOptions))
+app.use(
+    cors({
+      origin: ["http://127.0.0.1:3000", "http://localhost:3000"],
+      methods: "GET, POST, PATCH, DELETE, PUT",
+      credentials: true,
+    })
+  )
+  
+  app.use((req, res, next) => {
+    res.header("Access-Control-Allow-Origin", "http://localhost:3000")
+    next()
+  })
+
+app.use(express.json())
+
+app.use(cookieParser())
+
+app.use('/', express.static(path.join(__dirname, 'public')))
+
+app.use('/', require('./routes/root'))
+app.use('/auth', require('./routes/authRoutes'))
+app.use('/users', require('./routes/userRoutes'))
+app.use('/notes', require('./routes/noteRoutes'))
+
+app.all('*', (req, res) => {
+    res.status(404)
+    if (req.accepts('html')) {
+        res.sendFile(path.join(__dirname, 'views', '404.html'))
+    } else if (req.accepts('json')) {
+        res.json({ message: '404 Not Found' })
+    } else {
+        res.type('txt').send('404 Not Found')
+    }
+})
+
+app.use(errorHandler)
+
+mongoose.connection.once('open', () => {
+    console.log('Connected to MongoDB')
+    app.listen(PORT, () => console.log(`Server running on port ${PORT}`))
+})
+
+mongoose.connection.on('error', err => {
+    console.log(err)
+    logEvents(`${err.no}: ${err.code}\t${err.syscall}\t${err.hostname}`, 'mongoErrLog.log')
+})
